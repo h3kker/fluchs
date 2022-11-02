738a04a91d0424e936fe215d709b72ed247d2036
Heinz Axelsson-Ekker <haxel.hekker@testha.se>
2022-11-02 14:52:27 +0100

    dummy actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,22 +11,23 @@ const { categories } = storeToRefs(categoriesStore);
   <div class="container">
     <div class="columns is-multiline">
       <div class="column is-one-third" v-for="cat in categories" :key="cat.id">
-        <router-link :to="{ name: 'cat', params: { id: cat.id } }">
           <div class="card">
-            <header class="card-header">
-              <div class="card-header-title level">
-                <div class="level-left">
-                  <div class="level-item">{{ cat.title }}</div>
-                </div>
-                <div class="level-right">
-                  <div class="level-item">
-                    <b-tag :type="cat.unread_feeds > 0 ? 'is-primary' : ''">{{
-                      cat.unread_feeds
-                    }}</b-tag>
+            <router-link :to="{ name: 'cat', params: { id: cat.id } }">
+              <header class="card-header">
+                <div class="card-header-title level">
+                  <div class="level-left">
+                    <div class="level-item">{{ cat.title }}</div>
+                  </div>
+                  <div class="level-right">
+                    <div class="level-item">
+                      <b-tag :type="cat.unread_feeds > 0 ? 'is-primary' : ''">{{
+                        cat.unread_feeds
+                      }}</b-tag>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </header>
+              </header>
+            </router-link>
             <div class="card-content">
               <div class="content">
                 <p>
@@ -37,8 +38,19 @@ const { categories } = storeToRefs(categoriesStore);
                 </p>
               </div>
             </div>
+            <footer class="card-footer">
+              <div class="card-footer-item">
+                <a>
+                  <b-icon icon="book-open-outline"></b-icon>
+                </a>
+              </div>
+              <div class="card-footer-item">
+                <a>
+                  <b-icon icon="check-all"></b-icon>
+                </a>
+              </div>
+            </footer>
           </div>
-        </router-link>
       </div>
     </div>
   </div>
